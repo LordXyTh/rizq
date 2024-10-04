00d0d43993410bdf26e00ac8297c660caba4b2cf
rizki <rqaiserr@gmail.com>
2024-10-04 19:36:04 +0500

    Add CalculateSharesView
    This commit adds a new view called CalculateSharesView to the investing app. The view handles a POST request and calculates the number of shares to buy based on the investment amount and the market cap of each stock. The calculated data is rendered using a partial template and returned as an HTML response.

--- rizq/investing/urls.py
+++ rizq/investing/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from rizq.investing.views import CalculateSharesView
 from rizq.investing.views import IndexStockListView
 
 app_name = "investing"
 
 urlpatterns = [
     path("", IndexStockListView.as_view(), name="stock-index"),
+    path("calculate-shares/", CalculateSharesView.as_view(), name="calculate_shares"),
 ]
--- rizq/investing/views.py
+++ rizq/investing/views.py
@@ -1,5 +1,8 @@
 from django.db.models import Sum
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 from django.views.generic import ListView
+from django.views.generic import View
 
 from rizq.data.models import IndexStock
 from rizq.investing.utils.weighted_marketcap import calculate_weighted_market_cap
@@ -17,3 +20,33 @@ class IndexStockListView(ListView):
         for stock in context["stocks"]:
             stock.weighted_market_cap = calculate_weighted_market_cap(stock.market_cap, total_market_cap)
         return context
+
+
+class CalculateSharesView(View):
+    def post(self, request, *args, **kwargs):
+        investment_amount = float(request.POST.get("investment_amount", 0))
+        stocks = IndexStock.objects.filter(active=True).order_by("-market_cap")
+        total_market_cap = stocks.aggregate(total=Sum("market_cap"))["total"]
+
+        # Create a custom list to hold stock data, including calculated attributes
+        stock_data_list = []
+
+        for stock in stocks:
+            # Calculate weighted market cap
+            weighted_market_cap = calculate_weighted_market_cap(stock.market_cap, total_market_cap)
+            allocated_amount = (weighted_market_cap / 100) * investment_amount
+            # Calculate shares to buy (no fractional shares)
+            shares_to_buy = int(allocated_amount / stock.price)
+            # Add stock data to the custom list
+            stock_data = {
+                "symbol": stock.symbol,
+                "name": stock.name,
+                "price": stock.price,
+                "market_cap": stock.market_cap,
+                "weighted_market_cap": weighted_market_cap,
+                "shares_to_buy": shares_to_buy,
+            }
+            stock_data_list.append(stock_data)
+
+        html = render_to_string("investing/partials/investment_suggestions.html", {"stocks": stock_data_list})
+        return HttpResponse(html)
